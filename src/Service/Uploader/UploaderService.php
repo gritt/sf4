84648425266ab131a81b5c99efb1e102703d9e7b
Gilvan <gilvanritter@gmail.com>
2018-10-27 02:24:03 -0300

    created uploader service class

---
+++ src/Service/Uploader/UploaderService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Service\Uploader;
+
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class UploaderService
+ * @package App\Service\Uploader
+ */
+class UploaderService
+{
+    public function upload(Request $request): string
+    {
+        // TODO @gritt, implement uploader service
+    }
+}
